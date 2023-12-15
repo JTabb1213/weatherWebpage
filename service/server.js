670695065199baa62c47e47db743789b41b1ead8
Jack Tabb <jacktabb@Jacks-MacBook-Pro-2.local>
2023-12-15 13:50:43 -0600

    I added a route parameter so you can change the city name

--- service/server.js
+++ service/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+ 
 const API_KEY = 'cc6af3ddc3889911b9d0f06a9ce2734a';
+
 app.get('/', (req, res) => {
   axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=chicago&appId=${API_KEY}`).then(result => {
     res.json(result.data);
   });
 
 });
+
+app.get('/:id', (req, res) => {
+	const city = req.params.id;
+	axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=${city}&appId=${API_KEY}`).then(result => {
+    		res.json(result.data);
+  	});
+})
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
