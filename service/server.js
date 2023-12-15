c6937e6b91a6de5943ec5bb368a965a3fd01faa3
mtabb13 <mtabb13@gmail.com>
2023-12-15 13:07:44 -0500

    Add api integration

--- service/server.js
+++ service/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
-
+const API_KEY = 'cc6af3ddc3889911b9d0f06a9ce2734a';
 app.get('/', (req, res) => {
-  res.send('Successful response.');
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=chicago&appId=${API_KEY}`).then(result => {
+    res.json(result.data);
+  });
+
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
