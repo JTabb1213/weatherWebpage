1c1ca6f5cb9a591bd94684e102f10f94b96524f0
mtabb13 <mtabb13@gmail.com>
2023-12-17 11:08:50 -0600

    Add query parms and rewire page to call new web service

--- service/server.js
+++ service/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const axios = require('axios');
+var cors = require('cors')
 const app = express();
+var corsOptions = {
+  origin: 'http://localhost:8080'
+}
+app.use(cors(corsOptions));
+
 const API_KEY = 'cc6af3ddc3889911b9d0f06a9ce2734a';
-app.get('/', (req, res) => {
-  axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=chicago&appId=${API_KEY}`).then(result => {
+app.get('/api/weather', (req, res) => {
+  const city = req.query.city;
+  const units = req.query.units;
+  if (!city) {
+    return res.status(400).send('The request is missing a city');
+  }
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?&units=${units}&q=${city}&appId=${API_KEY}`).then(result => {
     res.json(result.data);
   });
 
