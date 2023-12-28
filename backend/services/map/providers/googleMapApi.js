768eccd286fb33910b2e5f4b0176c23641890d31
mtabb13 <mtabb13@gmail.com>
2023-12-27 19:28:56 -0600

    Fix code

--- backend/services/map/providers/googleMapApi.js
+++ backend/services/map/providers/googleMapApi.js
@@ -5,12 +5,6 @@ async function getMap(city) {
     const geocode = await location.getCoordinates(city);
     const geometry = geocode.results[0].geometry;
     return `https://maps.googleapis.com/maps/api/staticmap?center=${geometry.location.lat},${geometry.location.lng}&key=${apiKey}&size=600x600&zoom=15`
-    // return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${apiKey}`).then(result => {
-    //     console.log(result.data);
-    //
-    //     return result.data;
-    // });
-
 }
 
 module.exports = {
