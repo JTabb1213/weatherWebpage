b3a69694bd27f437f373d5e69411bf3bb0727cff
Jack Tabb <jacktabb@Jacks-MacBook-Pro-2.local>
2024-01-03 17:59:20 -0600

    Switched to redis

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const basicAuth = require('express-basic-auth');
+//const basicAuth = require('express-basic-auth');
 const weatherService = require('./services/weather');
 const locationService = require('./services/location');
 const mapService = require('./services/map');
 const session = require('express-session');
 const cors = require('cors');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
+var bodyParser = require('body-parser');
+
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-// This is a very primitive user storage implementation
+
 const USERS = {
     foo: 'bar'
 }
+
 app.use(
     cors({
         origin: "http://localhost:3000",
@@ -19,33 +22,55 @@ app.use(
     })
 );
 
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// enable this if you run behind a proxy (e.g. nginx)
+app.set('trust proxy', 1);
+
+const RedisStore = connectRedis(session)
+//Configure redis client
+const redisClient = redis.createClient({
+    host: 'localhost',
+    port: 6379
+})
+
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+});
+
+//Configure session middleware
 app.use(session({
-    secret: 'my secret',
-    saveUninitialized: true,
+    store: new RedisStore({ client: redisClient }),
+    secret: 'secret$%^134',
     resave: false,
-    proxy: true,
-    name: "jacks-weather-app",
+    saveUninitialized: false,
     cookie: {
-        httpOnly: true,
+        secure: false, // if true only transmit cookie over https
+        httpOnly: false, // if true prevent client side JS from reading the cookie 
+        maxAge: 1000 * 60 * 10 // session max age in miliseconds
     }
-}));
+}))
 
 function isLoggedIn(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.status(401).json({message: 'Not authorized'});
+        res.status(401).json({ message: 'Not authorized' });
     }
 }
 
 app.get('/api/map', isLoggedIn, (req, res) => {
-    const {city} = req.query
+    const { city } = req.query
     if (!city) {
         return res.status(400).send('The request is missing a city');
     }
     mapService.getMapUrl(city).then(result => {
         if (!result) {
-            return res.status(404).json({message: 'City not found, or there were more than 1 candidates'});
+            return res.status(404).json({ message: 'City not found, or there were more than 1 candidates' });
         }
         res.json({
             mapUrl: result
@@ -64,7 +89,7 @@ app.get('/api/weather', isLoggedIn, (req, res) => {
     weatherService.getWeather(city, units).then(result => {
         res.json(result);
     }).catch(err => {
-        res.status(err.response.status).json({message: err.response.statusText});
+        res.status(err.response.status).json({ message: err.response.statusText });
     });
 });
 
@@ -73,7 +98,7 @@ app.get('/api/geolocation', isLoggedIn, (req, res) => {
     locationService.getCoordinates(city).then(result => {
         res.json(result);
     }).catch(err => {
-        res.status(err.response.status).json({message: err.response.statusText});
+        res.status(err.response.status).json({ message: err.response.statusText });
     })
 });
 
@@ -81,17 +106,27 @@ app.post('/api/login', (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
     if (!username || !password) {
-        return res.status(400).json({message: 'Missing username or password'});
+        return res.status(400).json({ message: 'Missing username or password' });
     }
     const storedPass = USERS[username];
     if (!storedPass) {
-        return res.status(400).json({message: 'User not found'});
+        return res.status(400).json({ message: 'User not found' });
     }
     if (storedPass !== password) {
-        return res.status(400).json({message: 'Password does not match'});
+        return res.status(400).json({ message: 'Password does not match' });
     }
     req.session.user = username;
     res.status(200).json({});
 });
+
+app.post('/api/logout', (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) {
+            return console.log(err);
+        }
+        //res.redirect("/")
+        res.status(200).json({});
+    });
+})
 
 app.listen(4000, () => console.log('Example app is listening on port 4000.'));
