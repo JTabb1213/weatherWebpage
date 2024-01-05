635aaef6706b74d50309bb0dd1d9c08818809e9b
mtabb13 <mtabb13@gmail.com>
2024-01-05 11:29:22 -0500

    Add mui styles

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,8 @@
-import { Button } from "@mui/material";
-import { useState } from "react";
+import {Alert, Box, Button, Grid, TextField} from "@mui/material";
+import {useState} from "react";
 import styles from '../css/login.module.css';
-import { useHttpClient } from "../HttpClient";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import {useHttpClient} from "../HttpClient";
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 export default function Login() {
     const httpClient = useHttpClient();
@@ -10,8 +10,7 @@ export default function Login() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    const [emailError, setEmailError] = useState("")
-    const [passwordError, setPasswordError] = useState("")
+    const [error, setError] = useState();
 
     const onButtonClick = () => {
         httpClient.post('/api/login', {
@@ -25,8 +24,7 @@ export default function Login() {
             const originalUrl = searchParams.get('original_url') || '/';
             navigate(originalUrl);
         }).catch(err => {
-            setEmailError(err.response.data.message);
-            setPasswordError(err.response.data.message)
+            setError(err.response.data.message);
         })
     }
 
@@ -41,9 +39,7 @@ export default function Login() {
         }).then(result => {
             if (result.data === 'user not added') {
                 console.log('User not created');
-            }
-
-            else {
+            } else {
                 console.log('user created');
             }
         }).catch(err => {
@@ -52,43 +48,46 @@ export default function Login() {
     }
 
     return <div className={styles.mainContainer}>
-        <div className={styles.titleContainer}>
-            <div>Login</div>
-        </div>
-        <br />
-        <div className={styles.inputContainer}>
-            <input
-                value={username}
-                placeholder="Username"
-                onChange={ev => setUsername(ev.target.value)}
-                className={styles.inputBox} />
-            <label className={styles.errorLabel}>{emailError}</label>
-        </div>
-        <br />
-        <div className={styles.inputContainer}>
-            <input
-                value={password}
-                placeholder="Password"
-                onChange={ev => setPassword(ev.target.value)}
-                className={styles.inputBox} />
-            <label className={styles.errorLabel}>{passwordError}</label>
-        </div>
-        <br />
-        <div className={styles.inputContainer}>
-            <input
-                className={styles.inputButton}
-                type="button"
-                onClick={onButtonClick}
-                value={"Log in"} />
-        </div>
-        <br />
-        <div className={styles.inputContainer}>
-            <input
-                className={styles.inputButton}
-                type="button"
-                onClick={onCreateUser}
-                value={"create user"} />
-        </div>
+        <Box sx={{width: { xs: "75%", md: '30%' }}}>
+            <Grid container
+                  alignItems="center"
+                  justifyContent="center"
+                  spacing={2}>
+                <Grid item align="center"
+                      xs={12}>
+                    <img src="/lock.png"/>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <div className={styles.title}>Sign in</div>
+                </Grid>
+
+                {error && <Grid item xs={12}>
+                    <Alert severity="error">{error}</Alert>
+                </Grid>}
+
+                <Grid item xs={12}>
+                    <TextField fullWidth defaultValue={username}
+                               onChange={ev => setUsername(ev.target.value)}
+                               label="Username" variant="outlined" onChange={ev => setUsername(ev.target.value)}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth defaultValue={password}
+                               onChange={ev => setPassword(ev.target.value)}
+                               label="Password" variant="outlined"/>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button fullWidth variant="contained" onClick={onButtonClick}>SIGN IN</Button>
+                </Grid>
+                <Grid container item xs={12} direction="row">
+                    <Grid item xs={12} md={6} align="start">
+                        <a onClick={() => alert('TODO: Need to handle forgot password')}>Forgot password?</a>
+                    </Grid>
+                    <Grid item xs={12} md={6} align="end">
+                        <a onClick={onCreateUser}>Don't have an account? Sign Up</a>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
     </div>
 
 
